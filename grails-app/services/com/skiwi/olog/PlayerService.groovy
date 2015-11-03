6bf8f5ce6fa9d942712a3d08eb51440530ab7a4d
skiwi <frank_skiwi@hotmail.com>
2015-11-03 16:32:01 +0100

    Added PlayerService to create and retrieve players and to get the player alias at a given time as well as update the player alias for a given time.

---
+++ grails-app/services/com/skiwi/olog/PlayerService.groovy
@@ -0,0 +1,35 @@
+package com.skiwi.olog
+
+import grails.transaction.Transactional
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+
+@Transactional
+class PlayerService {
+    Player getPlayer(Integer playerId, String name) {
+        Player.findByPlayerId(playerId) ?: createPlayer(playerId, name)
+    }
+
+    Player createPlayer(Integer playerId, String name) {
+        def player = new Player(playerId: playerId)
+        player.addToAliases(new PlayerAlias(name: name, begin: Instant.ofEpochMilli(0), end: Instant.now().plus(50000, ChronoUnit.DAYS)))
+        player.save()
+    }
+
+    void updatePlayerName(Player player, String name, Instant instant) {
+        int index = player.aliases.findIndexOf { it.inInterval(instant) }
+        def playerAlias = player.aliases[index]
+        if (playerAlias.name != name) {
+            def oldEnd = playerAlias.end
+            playerAlias.end = instant
+            def newPlayerAlias = new PlayerAlias(name: name, begin: instant, end: oldEnd)
+            player.addToAliases(newPlayerAlias)
+            player.save()
+        }
+    }
+
+    PlayerAlias getPlayerAlias(Player player, Instant instant) {
+        player.aliases.find { it.inInterval(instant) }
+    }
+}
---
+++ src/test/groovy/com/skiwi/olog/PlayerServiceSpec.groovy
@@ -0,0 +1,93 @@
+package com.skiwi.olog
+
+import grails.test.mixin.Mock
+import grails.test.mixin.TestFor
+import grails.validation.ValidationException
+import spock.lang.Specification
+
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.stream.Stream
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(PlayerService)
+@Mock([Player, PlayerAlias])
+class PlayerServiceSpec extends Specification {
+    def playerId = 103168
+    def playerName = "skiwi"
+    Instant now
+
+    def setup() {
+        now = Instant.now()
+    }
+
+    def cleanup() {
+    }
+
+    void "test get player"() {
+        when: "get non-existing player"
+        def createdPlayer = service.getPlayer(playerId, playerName)
+
+        then: "player should be created"
+        createdPlayer
+        createdPlayer.playerId == playerId
+        service.getPlayerAlias(createdPlayer, now).name == playerName
+        Player.findByPlayerId(playerId) == createdPlayer
+
+        when: "get existing player"
+        def existingPlayer = service.getPlayer(playerId, playerName)
+
+        then: "player should exist"
+        existingPlayer
+        existingPlayer.playerId == playerId
+        service.getPlayerAlias(existingPlayer, now).name == playerName
+        Player.findByPlayerId(playerId) == existingPlayer
+    }
+
+    void "test create player"() {
+        when: "create new player"
+        def createdPlayer = service.createPlayer(playerId, playerName)
+
+        then: "player should be created"
+        createdPlayer
+        createdPlayer.playerId == playerId
+        service.getPlayerAlias(createdPlayer, Instant.now()).name == playerName
+        Player.findByPlayerId(playerId) == createdPlayer
+
+        when: "create other player with same playerId"
+        service.createPlayer(playerId, playerName)
+
+        then:
+        def ex = thrown(ValidationException)
+        ex.errors.allErrors.stream().flatMap({objectError -> objectError.codes.toList().stream()}).any { it == "unique" }
+    }
+
+    void "test update player name and get player alias"() {
+        given: "a player"
+        def player = service.createPlayer(playerId, playerName)
+        def newPlayerName = "skiwi2"
+
+        when: "player has been seen with same name"
+        service.updatePlayerName(player, playerName, now)
+
+        then: "player does not have a new alias"
+        player.aliases.size() == 1
+        service.getPlayerAlias(player, now.minus(1, ChronoUnit.HOURS)).name == playerName
+        service.getPlayerAlias(player, now).name == playerName
+        service.getPlayerAlias(player, now.plus(1, ChronoUnit.HOURS)).name == playerName
+
+        when: "player has been seen with a different name"
+        def updateInstant = now.minus(2, ChronoUnit.HOURS)
+        service.updatePlayerName(player, newPlayerName, updateInstant)
+
+        then: "player has a new alias"
+        player.aliases.size() == 2
+        service.getPlayerAlias(player, now.minus(4, ChronoUnit.HOURS)).name == playerName
+        service.getPlayerAlias(player, now.minus(2, ChronoUnit.HOURS)).name == newPlayerName
+        service.getPlayerAlias(player, now.plus(2, ChronoUnit.HOURS)).name == newPlayerName
+        service.getPlayerAlias(player, now.minus(4, ChronoUnit.HOURS)).end == updateInstant
+        service.getPlayerAlias(player, now.minus(2, ChronoUnit.HOURS)).begin == updateInstant
+    }
+}
