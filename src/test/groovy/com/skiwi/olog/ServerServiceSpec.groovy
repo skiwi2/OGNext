a7f71c9ccb974cc509c0fc27c80aab366220a132
skiwi <frank_skiwi@hotmail.com>
2015-11-05 15:55:23 +0100

    Added ServerService to get or create servers.

---
+++ src/test/groovy/com/skiwi/olog/ServerServiceSpec.groovy
@@ -0,0 +1,36 @@
+package com.skiwi.olog
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ServerService)
+class ServerServiceSpec extends Specification {
+    def serverCountryCode = "en"
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test get or create server"() {
+        when: "get non-existing server"
+        def createdServer = service.getOrCreateServer(serverCountryCode)
+
+        then: "server should be created"
+        createdServer
+        createdServer.countryCode == serverCountryCode
+        Server.findByCountryCode(serverCountryCode) == createdServer
+
+        when: "get existing server"
+        def existingServer = service.getOrCreateServer(serverCountryCode)
+
+        then: "server should exist"
+        existingServer
+        existingServer.countryCode == serverCountryCode
+        Server.findByCountryCode(serverCountryCode) == existingServer
+    }
+}
