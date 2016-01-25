16ac3eec0b90aebc0021aa4d7b5d07628bb0209e
skiwi <frank_skiwi@hotmail.com>
2016-01-25 14:30:17 +0100

    Removed failing stub test.

--- src/test/groovy/com/ognext/PlanetControllerSpec.groovy
+++ src/test/groovy/com/ognext/PlanetControllerSpec.groovy
@@ -15,8 +15,5 @@ class PlanetControllerSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
-        expect: "fix me"
-        true == false
-    }
+    //TODO write tests
 }
